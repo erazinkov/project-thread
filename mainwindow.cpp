b04bfdb0aa437ba5fde4f79671e3b19877dcd6d4
erazinkov <egorrazinkov@gmail.com>
2025-09-18 12:10:40 +0300

    small fix

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -41,7 +41,6 @@ MainWindow::MainWindow(QWidget *parent)
             str.append(" }");
             qDebug() << str;
         }
-
     });
 }
 
@@ -54,7 +53,7 @@ void MainWindow::handleClickedStart()
 {
     m_list->clear();
     QQueue<MyTask *> myTasks;
-    const int n{15};
+    const int n{32};
     m_progressBar->setMinimum(0);
     m_progressBar->setMaximum(n);
     m_progressBar->setValue(0);
@@ -63,10 +62,12 @@ void MainWindow::handleClickedStart()
         MyTask *myTask = new MyTask();
         myTasks.enqueue(myTask);
         myTask->setAutoDelete(true);
+
     }
     QThreadPool *threadPool = QThreadPool::globalInstance();
+    qDebug() << QThread::idealThreadCount();
     threadPool->setMaxThreadCount(QThread::idealThreadCount());
-    for (const auto &item : myTasks)
+    for (const auto &item: myTasks)
     {
         MyTask *newTask = myTasks.dequeue();
         threadPool->start(newTask);
@@ -75,7 +76,7 @@ void MainWindow::handleClickedStart()
             m_progressBar->setValue(++v);
             m_list->push_back(c);
         }, Qt::QueuedConnection);
-        connect(m_buttonStop, &QPushButton::clicked, newTask, &MyTask::stop, Qt::QueuedConnection);
+        connect(m_buttonStop, &QPushButton::clicked, newTask, &MyTask::stop);
     }
 }
 
